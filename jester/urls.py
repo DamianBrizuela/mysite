76ad304f29c82f5edeaa7076df382fdd21e7762b
Damián Brizuela <dbrizuela@zoonsuite.com>
2025-09-05 14:16:23 -0300

    add urls

--- jester/urls.py
+++ jester/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('intro/', views.hello, name='MWAM')
 ]
--- jester/views.py
+++ jester/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def landing_page(request):
     return HttpResponse("<h1>Bienvenido a Jester</h1><p>Landing page en construcción 🚀</p>")
+
+
+def hello(request):
+    return HttpResponse('<h2>Esta es otra vista </h2><p>Dentro de Jester!!!</p>')
